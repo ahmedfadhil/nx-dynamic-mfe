e64941105061930085cebb6c88b90b5cec7da0a8
ahmed jumaah <ahmed.jumaah@spindox.it>
2023-08-16 23:52:26 +0200

    feat(MX1212): this is the login page
    the login contains the form and its code
    
    close feat MX1212

--- libs/login/login-form/src/lib/login-form/login-form.component.ts
+++ libs/login/login-form/src/lib/login-form/login-form.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { UserService } from '@nx-dynamic-mf/shared/data-access-user';
 
 @Component({
   selector: 'nx-dynamic-mf-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  username = '';
+  password = '';
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private userService: UserService) {}
+
+  login() {
+    this.userService.checkCredentials(this.username, this.password);
+  }
+}
